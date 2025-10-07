8b5740451294a66087e4a5d858f508958f739539
Breno PZ <d2023001951@unifei.edu.br>
2025-10-07 14:44:03 -0300

    funcionalidades de login adicionadas

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,28 +1,62 @@
 import React, {useState} from 'react';
-import { View, Image, TouchableOpacity } from 'react-native';
+import { View, Image, TouchableOpacity, Alert } from 'react-native';
 import { Background, BotaoGradientBackground, ButtonText, ButtonTouchable,  LabelInput,  MiniTexto, SubTitulo, Texto, TextoInput} from '../../Styleguide/styles';
 import styled, { withTheme } from 'styled-components/native';
+
 import logo from '../../assets/Logo.png';
 import vector from '../../assets/Vector.png';
 import checkboxTrue from '../../assets/Checkbox.png'
 import checkboxFalse from '../../assets/Subtract.png'
+import error from '../../assets/Error icon.png'
 
-export default function Inicial(){
+import { useNavigation } from '@react-navigation/native';
+
+export default function Login(){
+
+const navigation = useNavigation();
 
 const [selecionado, setSelecionado] = useState(null);
+const [email, setEmail] = useState('');
+const [senha, setSenha] = useState('');
+
+const [mensagem, setMensagem] = useState('');
 
 const handleSelect = () => {
         setSelecionado(!selecionado); 
+    };
+
+    const USUARIO_TESTE = 'breno@gmail.com';
+    const SENHA_TESTE = '12345';
+    
+    // Função principal de Login
+    const handleLogin = () => {
+        // 1. Validação básica (campos vazios)
+        if (!email.trim() || !senha.trim()) {
+            setMensagem("Por favor, preencha todos os campos.");
+            return;
+        }
+        setTimeout(() => {
+
+            if (email === USUARIO_TESTE && senha === SENHA_TESTE) {
+                setMensagem('');
+                Alert.alert("Sucesso", "Login simulado OK! Redirecionando...");
+                navigation.navigate('Home')
+            } else {
+                setMensagem("E-mail ou senha inválidos. Tente novamente.")
+            }
+        }, 2000); // Espera 2000ms (2 segundos)
     };
 
     return(
 
     <Background>
         <View style={{marginHorizontal:30, marginTop:25, justifyContent:'space-between', flexDirection:'row', alignItems:'center'}}>
-            <Image source={vector} style={{width:8, height:16}}></Image>
+            <TouchableOpacity onPress={ () => navigation.navigate('Inicial') }>
+                <Image source={vector} style={{width:8, height:16}}></Image>
+            </TouchableOpacity>
             <View style={{gap:8, flexDirection:'row'}}>
                 <MiniTexto style={{color:'#F0F2F5', alignSelf:'center', marginTop:4}}> Não tem conta ainda? </MiniTexto>
-                <BotaoCadastrar>
+                <BotaoCadastrar onPress={ () => navigation.navigate('Cadastro') }>
                     <Texto style={{color:'#F0F2F5'}}>Cadastre-se</Texto>
                 </BotaoCadastrar>
             </View>
@@ -62,16 +96,39 @@ const handleSelect = () => {
                             <MiniTexto style={{marginBottom:-12}}>
                                 Email
                             </MiniTexto>
-                            <TextoInput >
+                            <TextoInput 
+                                placeholder='Digite aqui'
+                                value={email}
+                                onChangeText={setEmail}
+                                >
                             </TextoInput>
                         </LabelInput>
 
                         <LabelInput>
-                            <MiniTexto style={{marginBottom:-12}}>Senha
+                            <MiniTexto style={{marginBottom:-12}}>
+                                Senha
                             </MiniTexto>
-                            <TextoInput >
+                            <TextoInput 
+                                placeholder='Digite aqui'
+                                value={senha}
+                                onChangeText={setSenha}
+                                secureTextEntry={true}
+                                >
                             </TextoInput>
                         </LabelInput>
+
+                        {mensagem !== '' && (
+                            <View style={{flexDirection:'row',position: 'absolute',bottom: 118,left: 4, gap:3}}>
+                                <Image source={error} style={{width:11, height:11, resizeMode:'contain', marginTop:3}}></Image>               
+                                <MiniTexto
+                                style={{
+                                    color: 'red',
+                                }}
+                                >
+                                {mensagem}
+                                </MiniTexto>
+                            </View>
+                        )}
 
                         <MiniTexto style={{alignSelf:'flex-end'}}>
                             Esqueceu a senha?
@@ -79,7 +136,7 @@ const handleSelect = () => {
 
                         <BotaoGradientBackground>
                             <ButtonTouchable>
-                                <ButtonText>
+                                <ButtonText onPress={handleLogin}>
                                     Confirmar
                                 </ButtonText>
                             </ButtonTouchable>
@@ -143,8 +200,8 @@ opacity: 1;
 border-top-left-radius: 32px;
 border-top-right-radius: 32px;
 position:absolute;
-bottom: 10;
-background: #FFFFFF33;
+bottom: 13;
+background: #F0F2F580;
 zIndex:0;
 `
 const BotaoCadastrar = styled.TouchableOpacity`
